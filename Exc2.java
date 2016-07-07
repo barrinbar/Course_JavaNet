38ee4818d8dd9dbd535eb91c52ae206b23c34365
omerel <elomer@gmail.com>
2016-07-07 20:17:55 +0300

    Update Exc2.java

--- Exc2.java
+++ Exc2.java
@@ -1,59 +1,61 @@
+package Week_2;
+
 import java.util.concurrent.Semaphore;
 
 public class Exc2 {
-	static String obj1 = new String("First");
-	static String obj2 = new String("Second");
-	
+	public static String obj1 = new String("First");
+	public static String obj2 = new String("Second");
+	public static Semaphore semaphore = new Semaphore(1);
+
 	public Exc2() {
-		MyThread thr1 = new MyThread(0, obj1, obj2);
-		MyThread thr2 = new MyThread(1, obj2, obj1);
-		
+		MyThread_1 thr1 = new MyThread_1();
+		MyThread_2 thr2 = new MyThread_2();
 		thr1.start();
 		thr2.start();
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-
 		Exc2 exc = new Exc2();
-		
 	}
-}
 
-class MyThread extends Thread {
-	
-	int m_id;
-	String obj1;
-	String obj2;
-	
-	// CTor
-	public MyThread(int sid, String str1, String str2) {
-		m_id = sid;
-		obj1 = str1;
-		obj2 = str2;
-	}
-	
-	@Override
-	public void run() {
-		System.out.println("Thread #" + m_id + " trying to lock " + obj1 + " object");
-		for(int i=0; i<3600000; i++);
-		synchronized (obj1) {
-			System.out.println("Thread #" + m_id + " has locked " + obj1 + " object");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	class MyThread_1 extends Thread {
+		// CTor
+		public MyThread_1() {
 		}
-		
-		System.out.println("Thread #" + m_id + " trying to lock " + obj2 + " object");
-		for(int i=0; i<3600000; i++);
-		synchronized (obj2) {
-			System.out.println("Thread #" + m_id + " has locked " + obj2 + " object");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+
+		@Override
+		public void run() {
+
+			System.out.println("Thread 1" + " trying to lock " + obj1 + " object");
+			synchronized (obj1) {
+				System.out.println("Thread 1" + " has locked " + obj1 + " object");
+				// Thread.sleep(1000);
+				System.out.println("Thread 1" + " trying to lock " + obj2 + " object");
+				synchronized (obj2) {
+					System.out.println("Thread 1" + " has locked " + obj2 + " object");
+				}
 			}
+
+		}
+	}
+
+	class MyThread_2 extends Thread {
+		// CTor
+		public MyThread_2() {
+		}
+
+		@Override
+		public void run() {
+
+			System.out.println("Thread 2" + " trying to lock " + obj2 + " object");
+			synchronized (obj2) {
+				System.out.println("Thread 2" + " has locked " + obj2 + " object");
+				System.out.println("Thread 2" + " trying to lock " + obj1 + " object");
+				synchronized (obj1) {
+					System.out.println("Thread 2" + " has locked " + obj1 + " object");
+				}
+			}
+
 		}
 	}
 }
