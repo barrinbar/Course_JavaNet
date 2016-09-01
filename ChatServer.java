eac39646297e45de6557b219785d75a5558f8216
Barr Inbar <barrinbar@users.noreply.github.com>
2016-09-01 20:44:54 +0300

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -2,7 +2,14 @@ package chat;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ThreadFactory;
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -18,7 +25,8 @@ public class ChatServer extends Application { // Text area for displaying
 	private TextArea ta = new TextArea();
 	private ServerSocket serverSocket;
 	private Socket socket;
-	private ClientsList clients = new ClientsList();;
+	private ClientsList clients = new ClientsList();
+	private Map<String, ObjectOutputStream> clientOutStreams;
 
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) { // Create a scene and place it in
@@ -28,7 +36,7 @@ public class ChatServer extends Application { // Text area for displaying
 		primaryStage.setTitle("Chat Server"); // Set the stage title
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.show(); // Display the stage
-		primaryStage.setAlwaysOnTop(true);
+		primaryStage.setAlwaysOnTop(false);
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 			public void handle(WindowEvent event) {
 				Platform.exit();
@@ -38,6 +46,7 @@ public class ChatServer extends Application { // Text area for displaying
 		new Thread(() -> {
 			try { // Create a server socket
 				serverSocket = new ServerSocket(8000);
+				clientOutStreams = new HashMap<>();
 				Platform.runLater(() -> {
 					ta.appendText("Server started at " + new Date() + '\n');
 				});
@@ -48,8 +57,47 @@ public class ChatServer extends Application { // Text area for displaying
 					Platform.runLater(() -> { // Display the client info
 						ta.appendText(socket.toString() + '\n');
 					});
-					// Create and start a new thread for the connection
-					new Thread(new HandleAClient(socket)).start();
+					
+					ObjectOutputStream toClient = new ObjectOutputStream(socket.getOutputStream());
+					ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
+
+					// Receive client name
+					String clientName;
+					try {
+						clientName = (String) fromClient.readObject();
+						
+						ClientKey clientKey = new ClientKey(socket.getPort(), socket.getInetAddress().getHostName());
+	
+						if (clientOutStreams.containsKey(clientName)) {
+							toClient.writeObject("Name already taken");
+						}
+						else {
+							// TODO: Collections.sort(clientOutStreams);
+						//if (!clients.clientExists(clientKey)) {
+							/*if (clients.nameExists(clientName)) {
+								// Inform the client by sending an empty clients list
+								toClient.writeObject(new ClientsList());
+							} else {
+								// Add the new client and send the updated list to the
+								// server
+								clients.addClient(clientKey, clientName);
+								clients.sortByValue();*/
+								clientOutStreams.put(clientName, toClient);
+/*								for (ClientKey client : clients.getClients().keySet()) {
+									clientOutStreams.get(client.getPort()).writeObject(clients);
+									Platform.runLater(() -> {
+										ta.appendText("Sending to " + client.getPort() + " ");
+									});
+								}	
+*/								Platform.runLater(() -> ta.appendText("\nNew client connected: " + clientName + "\n"));
+								// Create and start a new thread for the connection
+								new Thread(new HandleAClient(socket, clientName)).start();
+							//}
+						}
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			} catch (IOException ex) {
 			}
@@ -60,70 +108,63 @@ public class ChatServer extends Application { // Text area for displaying
 	class HandleAClient implements Runnable {
 		private Socket socket; // A connected socket
 		private String clientName = "";
-		private ClientKey clientKey = null;
 
 		/** Construct a thread */
-		public HandleAClient(Socket socket) {
+		public HandleAClient(Socket socket, String clientName) {
 			this.socket = socket;
+			this.clientName = clientName;
 		}
 
 		/** Run a thread */
 		@SuppressWarnings("resource")
 		public void run() {
 			try {
-				ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
-				ObjectOutputStream toClient = new ObjectOutputStream(socket.getOutputStream());
+				Platform.runLater(() -> {
+					ta.appendText("In thread of " + clientName +"\n");
+				});
+				SerClients ca = new SerClients(clientOutStreams.keySet());
+				// Send new clients list to all clients
+				for (ObjectOutputStream client : clientOutStreams.values()) {
+					client.writeObject(ca);
+				}	
+				// Continuously serve the client
+				while (true) {
+					ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
 
-				// Receive client name
-				clientName = (String) fromClient.readObject();
+					// Receive message from the client
+					Message message = (Message) fromClient.readObject();
 
-				clientKey = new ClientKey(socket.getPort(), socket.getInetAddress().getHostName());
-
-				if (!clients.clientExists(clientKey)) {
-					if (clients.nameExists(clientName)) {
-						// Inform the client by sending an empty clients list
-						toClient.writeObject(new ClientsList());
+					// Check if user wants to disconnect
+					if (message.toString() == "") {
+						Platform.runLater(() -> {
+							ta.appendText(clientName + " has left the chat.\n");
+						});
+						break;
 					} else {
-						// Add the new client and send the updated list to the
-						// server
-						clients.addClient(clientKey, clientName);
-						clients.sortByValue();
-						toClient.writeObject(clients);
-						Platform.runLater(() -> ta.appendText("New client connected: " + clientName + "\n"));
-					}
-
-					// Continuously serve the client
-					while (true) {
-						// Receive message from the client
-						Message message = (Message) fromClient.readObject();
-
-						// Check if user wants to disconnect
-						if (message.toString() == "") {
-							Platform.runLater(() -> {
-								ta.appendText(clientName + " has left the chat.\n");
-							});
-						} else {
-							// Send message to all clients
-							for (ClientKey client : clients.getClients().keySet()) {
-								Socket msgSocket = new Socket(client.getHostName(), client.getPort());
-								ObjectOutputStream outputToClient = new ObjectOutputStream(msgSocket.getOutputStream());
-								outputToClient.writeObject(message);
-
-								// TODO: send just to destination clients
-							}
-							Platform.runLater(() -> {
-								ta.appendText("Message from " + clientName + ": " + message + '\n');
-							});
+						
+						Platform.runLater(() -> {
+							ta.appendText("Sending message from " + clientName + ": "+ message.getMsg() +"\n");
+						});
+						// Send message to all clients
+						for (ObjectOutputStream client : clientOutStreams.values()) {
+							client.writeObject(message);
 						}
+						
+						Platform.runLater(() -> {
+							ta.appendText("\nMessage from " + clientName + ": " + message + " was sent \n");
+						});
 					}
 				}
 			} catch (SocketException ex) {
+				ex.printStackTrace();
 				try {
 					serverSocket.close();
 					socket.close();
 				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			} catch (IOException ex) {
+				ex.printStackTrace();
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
