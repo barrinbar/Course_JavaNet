ba117b07e6c27733d6fb80cb449a87b7a6ac5697
Barr Inbar <barrinbar@users.noreply.github.com>
2016-09-08 21:00:38 +0300

    Update ClockView.java

--- ClockView.java
+++ ClockView.java
@@ -1,21 +1,78 @@
 package MVC;
 
 
-import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class ClocksMVC extends Application
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ClockView extends Stage implements Comparable<ClockView>, ChangeListener<ClockView>
 {
-	public static void main(String[] args)
+
+	public Country country;
+	
+	public ClockView(Country country)
 	{
-		launch(args);
+		minHeightProperty().set(350);
+		minWidthProperty().set(350);
+		this.country = country;
+		ClockPane clock = new ClockPane();
+		int hour = clock.getHour() + country.getTimeZone();
+		if (hour >= 24) hour -= 24;
+		clock.setHour(hour);
+		
+		Label lblCurrentTime = 
+				new Label(String.format("%02d : %02d : 02d" ,
+						clock.getHour(), clock.getMinute(), clock.getSecond()));
+		
+		Label lblPopulation =
+				new Label ("Current population: " +
+					NumberFormat.getNumberInstance(Locale.US).format(country.getPopulation()));
+
+		Label lblCapital =
+				new Label (country.getCapital());
+
+		Label lblFunfact =
+				new Label (country.getInfo());
+		
+		ImageView imgFlag =
+				new ImageView(country.getImageURL());
+		
+		
 	}
 	
-	public void start(Stage primaryStage)
-	{
-		ClockModel model = new ClockModel();
-		ClockController controller = new ClockController(primaryStage);
-		controller.setModelListener(model);
-		model.setControllerListener(controller);
+	/*@Override
+	public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+		// TODO: implement
+		
+	}*/
+
+	@Override
+	public int compareTo(ClockView arg0) {
+		if (this.equals(arg0))
+			return 0;
+		else
+			return -1;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		ClockView cv = (ClockView)obj;
+		
+		if (cv.country.equals(this.country))
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public void changed(ObservableValue<? extends ClockView> observable, ClockView oldValue, ClockView newValue) {
+		country = newValue.country;
+	};
+	
 }
